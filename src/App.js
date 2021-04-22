f682f95c3e8f834d61d3269907eed6680002f8b3
joeyborai <joeyborai1@gmail.com>
2021-04-22 19:58:34 -0400

    First commit to portfolio repositorty.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import { SideProjects } from './Pages/side_projects';
+import { Home } from './Pages/home';
+import { Resume } from './Pages/resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/side_projects">Side Projects</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/side_projects">
+            <SideProjects/>
+          </Route>
+          <Route path="/resume">
+            <Resume/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/side_projects.js
@@ -0,0 +1,9 @@
+import { React } from 'react';
+
+export const SideProjects = (props) => {
+  return (
+    <div>
+      These projects are sick.
+    </div>
+  )
+}
